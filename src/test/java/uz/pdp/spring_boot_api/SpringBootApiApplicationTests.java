2a8640aaf73cbcc3b4821c4f85750a386f0507f0
Akhror <axror19977@gmail.com>
2023-04-07 12:27:42 +0500

    some changes

--- src/test/java/uz/pdp/spring_boot_api/SpringBootApiApplicationTests.java
+++ src/test/java/uz/pdp/spring_boot_api/SpringBootApiApplicationTests.java
@@ -1,5 +1,6 @@
 package uz.pdp.spring_boot_api;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class SpringBootApiApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertTrue(false);
     }
 
 }
